d521884e9a11abeb2864cd1ceac64c4a2579091f
PandaforFun <121799252+PandaforFun@users.noreply.github.com>
2024-04-26 15:52:27 +0530

    Create Basic_Datatypes.py

---
+++ Basic_Datatypes.py
@@ -0,0 +1,33 @@
+# Integer 
+roll_number = 69
+# Float 
+cgpa = 9.5 
+# String 
+name = "Vijay Mallya" 
+# Boolean 
+is_student = True 
+# List 
+favorite_subjects = ["Economics", "Business", "Flight"] 
+# Tuple 
+birthdate = (2,3,2016) 
+# Range 
+possible_ages = range(20, 30) 
+# Dictionary 
+student_info = {"name": "Vijay Mallya", "roll_number": 69} 
+# Set 
+favorite_numbers = {7, 15, 33} 
+# None 
+no_value = None 
+# Displaying all variables 
+print("roll_number:", roll_number) 
+print("cgpa:", cgpa) 
+print("name:", name) 
+print("is_student:", is_student) 
+print("favorite_subjects:", favorite_subjects) 
+print("birthdate:", birthdate) 
+print("possible_ages:", list(possible_ages))
+
+
+print("student_info:", student_info) 
+print("favorite_numbers:", favorite_numbers) 
+print("no_value:", no_value) 
