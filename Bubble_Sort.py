51a99179c02ddd77a43e1cd9f05c4c49e234ff6c
PandaforFun <121799252+PandaforFun@users.noreply.github.com>
2024-04-29 22:32:47 +0530

    Create Bubble_Sort.py

---
+++ Bubble_Sort.py
@@ -0,0 +1,12 @@
+def bubble_sort(arr):
+    print("Original array:", arr)
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    print("Sorted array (Bubble Sort):", arr)
+
+arr = [6,1,3,4,2,5]
+
+bubble_sort(arr.copy())
